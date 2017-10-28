71c2cc6b5e9b91e335a0d2affa90c72cd595d29f
Luis Fernando <lfbu.112@gmail.com>
2017-10-28 13:02:03 -0300

    tutorial with ethereum and truffle

---
+++ contracts/Adoption.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.4;
+
+/**
+ * The Adoption contract does this and that...
+ */
+contract Adoption {
+	address[16] public adopters;	
+
+	function adopt(uint petId) public returns (uint) {
+		require(petId >= 0 && petId <= 15);
+
+		adopters[petId] = msg.sender;
+
+		return petId;	
+	}
+	
+
+	function getAdopters() public returns (address[16]) {
+		return adopters;
+	}
+	
+}
